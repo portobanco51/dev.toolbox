334ac35f03900cd7bc87de797cbe008c09610338
Marcelo <marcelo011299@gmail.com>
2022-09-12 10:57:46 -0600

    seo styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <>
             <Box className='hero-banner' margin='auto' minWidth='80vw' height='50vh' display='grid' justifyContent='center' alignContent='center' >
                 <Player autoplay loop renderer='svg' src="https://assets7.lottiefiles.com/packages/lf20_tsdnk8ut.json" style={{ height: '300px', width: '300px' }} />
-                <Link underline='none' href='#tools' className='header' variant='h2' color='#e0fffb' letterSpacing='4px' >dev<span className='span' >.toolShack</span></Link>
+                <Link textAlign='center' underline='none' href='#tools' className='header' variant='h2' color='#e0fffb' letterSpacing='4px' >dev<span className='span' >.toolBox</span></Link>
             </Box>
         </>
     )
