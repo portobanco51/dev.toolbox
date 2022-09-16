3d561550b35ebe287c858fcbbbb633357961b546
Marcelo <marcelo011299@gmail.com>
2022-09-16 01:37:07 -0600

    Footer styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
         <Box display='flex' flexDirection='column' mb='2rem' mt='3rem' >
             <Typography textAlign='center' mx='auto' variant='h4' fontWeight='300' color='#e0fffb' letterSpacing='4px' >dev<span className='span-foot' >.toolBox</span></Typography>
             <Typography color='#e0fffb' mt='1rem' fontWeight='200' fontSize='1rem' textAlign='center' variant='h6'>A small toolbox to find everything and get started on any project quickly!</Typography>
+            <img className='metaImg' src="../assets/metaImg.png" alt="metaImg" />
         </Box>
     )
 }
