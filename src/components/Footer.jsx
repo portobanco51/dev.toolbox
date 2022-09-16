044847ed99d85137473550126eced97511302c46
Marcelo <marcelo011299@gmail.com>
2022-09-16 02:07:00 -0600

    meta tags final commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import { Box, Typography } from '@mui/material';
+import MetaImg from '../assets/metaImg.png'
 
 const Footer = () => {
     return (
         <Box display='flex' flexDirection='column' mb='2rem' mt='3rem' >
             <Typography textAlign='center' mx='auto' variant='h4' fontWeight='300' color='#e0fffb' letterSpacing='4px' >dev<span className='span-foot' >.toolBox</span></Typography>
             <Typography color='#e0fffb' mt='1rem' fontWeight='200' fontSize='1rem' textAlign='center' variant='h6'>A small toolbox to find everything and get started on any project quickly!</Typography>
-            <img className='metaImg' src="../assets/metaImg.png" alt="metaImg" />
+            <img className='metaImg' src={MetaImg} alt="metaImg" />
         </Box>
     )
 }
