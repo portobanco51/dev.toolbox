fcffbe8ffb573b7f55f91200fce09c157a6c4d66
Marcelo <marcelo011299@gmail.com>
2022-09-16 00:51:53 -0600

    final styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Box, Typography } from '@mui/material';
 
 const Footer = () => {
     return (
-        <Box display='flex' flexDirection='column' my='2rem' >
+        <Box display='flex' flexDirection='column' mb='2rem' mt='3rem' >
             <Typography textAlign='center' mx='auto' variant='h4' fontWeight='300' color='#e0fffb' letterSpacing='4px' >dev<span className='span-foot' >.toolBox</span></Typography>
             <Typography color='#e0fffb' mt='1rem' fontWeight='200' fontSize='1rem' textAlign='center' variant='h6'>A small toolbox to find everything and get started on any project quickly!</Typography>
         </Box>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,10 +5,10 @@ import WebCard from './WebCard'
 const List = ({ data, name, icon, id }) => {
     return (
         <Box id={id} >
-            <Typography letterSpacing='4px' variant='h3' fontSize='2.5rem' textAlign='center' color='#e0fffb' mt='2rem' >{icon} {name}<span className='span-list' >.tools</span> {icon}</Typography>
+            <Typography letterSpacing='4px' variant='h3' fontSize='2.5rem' textAlign='center' color='#e0fffb' my='2rem' >{icon} {name}<span className='span-list' >.tools</span> {icon}</Typography>
 
             {data.length === 0 ? <Loader />
-                : <Box my='2rem' mx='auto' maxWidth='80vw' flexWrap='wrap' display='flex' alignItems='center' alignContent='flex-start' justifyContent='center' gap='2rem' className="container-weblist">
+                : <Box mx='auto' maxWidth='80vw' flexWrap='wrap' display='flex' alignItems='center' alignContent='flex-start' justifyContent='center' gap='2rem' className="container-weblist">
                     {
                         data?.map((e, i) => (
                             <WebCard key={i} data={data[i]} />
